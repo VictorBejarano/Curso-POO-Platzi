039b41ea444f9d311bfb1b917e28900161390acd
Vikthor <alejandrobjrn4@gmail.com>
2020-01-19 14:00:33 -0500

    Inheritance in PHP

--- CursoPOOUber/PHP/car.php
+++ CursoPOOUber/PHP/car.php
@@ -1,8 +1,18 @@
 <?php
+require_once('account.php');
 class Car {
     private $id;
     private $license;
     private $driver;
     private $passenger;
+
+    public function __construct($license, $driver) {
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function printDataCar() {
+        echo "Licencia: $this->license Driver: ".$this->driver->name;
+    }
 }
 ?>
