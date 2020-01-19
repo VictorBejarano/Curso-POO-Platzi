00f2e4b2547a67b06c9dbc6a9f635ea280006f15
Vikthor <alejandrobjrn4@gmail.com>
2020-01-19 10:12:14 -0500

    Define constructor in Python car and account

--- CursoPOOUber/Python/car.py
+++ CursoPOOUber/Python/car.py
@@ -1,5 +1,11 @@
+from account import Account
+
 class Car:
     id          = int
     license     = str
-    drive       = str
-    passenger  = str
+    driver      = Account("","")
+    passenger   = str
+
+    def __init__(self, license, driver):
+        self.license    = license
+        self.driver     = driver
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,14 +1,13 @@
 from car import Car
+from account import Account
 
 if __name__ == '__main__':
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Martha"
+    car = Car("AMS234", Account("Andres Herrera", "AND876"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    car2 = Car("QWE567", Account("Martha", "MRT789"))
     print(vars(car2))
-     
+    print(vars(car2.driver))
